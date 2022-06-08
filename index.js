22c5e5449024e86189b2554fb25b3ee0084aad05
Laura Wolfart <laurawolfart@gmail.com>
2022-06-08 15:29:25 -0400

    editing rain size

--- index.js
+++ index.js
@@ -1,60 +1,59 @@
 AOS.init({once: true});
 
 const counterAnimation = (el, initialNum, finalNum, speed) => {
-    if (Number.isInteger(finalNum)) {
-      let interval = setInterval(function() {
-        el.innerHTML = initialNum;
-        (initialNum >= finalNum) ? clearInterval(interval) : '';
-        initialNum++;
-      }, speed);
-    }
-    else {
-      let intInitialNum = Math.floor(initialNum);
-      let intFinalNum = Math.floor(finalNum);
-      let interval = setInterval(function() {
-        el.innerHTML = intInitialNum;
-        if (intInitialNum >= intFinalNum) {
-          clearInterval(interval);
-          el.innerHTML = `${finalNum.toString()}`;
-        }
-        intInitialNum++;
-      }, speed);
-    }
+  if (Number.isInteger(finalNum)) {
+    let interval = setInterval(function() {
+      el.innerHTML = initialNum;
+      (initialNum >= finalNum) ? clearInterval(interval) : '';
+      initialNum++;
+    }, speed);
+  }
+  else {
+    let intInitialNum = Math.floor(initialNum);
+    let intFinalNum = Math.floor(finalNum);
+    let interval = setInterval(function() {
+      el.innerHTML = intInitialNum;
+      if (intInitialNum >= intFinalNum) {
+        clearInterval(interval);
+        el.innerHTML = `${finalNum.toString()}`;
+      }
+      intInitialNum++;
+    }, speed);
+  }
 }
 
 function isInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+  const rect = el.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 }
 
 var bar = new ProgressBar.Circle(container, {
-    strokeWidth: 7,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#11BAE9',
-    trailColor: '#E2E2E2',
-    trailWidth: 7,
-    svgStyle: null
+  strokeWidth: 7,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#11BAE9',
+  trailColor: '#E2E2E2',
+  trailWidth: 7,
+  svgStyle: null
 });
 
 const box2 = document.querySelector('#animate-stats2');
 const juggle = document.querySelector('#animate-juggle')
 
 document.addEventListener('scroll', function () {
-  console.log(juggle.classList)
-      if (isInViewport(juggle) && juggle.classList.value ==('') ) {
-        let img = new Image();
-        img.src = './images/Juggler_v4.gif'
-        img.className = 'gif'
-        juggle.prepend(img);
-        juggle.classList.add('hasGif');
-      }
-      if (isInViewport(box2)) {
-        bar.animate(0.6);  // Number from 0.0 to 1.0
-    }
+  if (isInViewport(juggle) && juggle.classList.value ==('') ) {
+    let img = new Image();
+    img.src = './images/Juggler_v4.gif'
+    img.className = 'gif'
+    juggle.prepend(img);
+    juggle.classList.add('hasGif');
+  }
+  if (isInViewport(box2)) {
+    bar.animate(0.6);  // Number from 0.0 to 1.0
+}
 });
